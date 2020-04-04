429c18e655cbff04c79bc7739366202046154430
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-04-04 16:23:17 -0400

    Animate the modal and add a title

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from "react";
-import { Dialog } from "@reach/dialog";
+import { Dialog, DialogOverlay, DialogContent } from "@reach/dialog";
+import { AnimatePresence, motion } from "framer-motion";
 import { css } from "@emotion/core";
 import { Theme } from "../../styles/theme";
 
@@ -15,35 +16,83 @@ export const useModal = (): [boolean, () => void, () => void] => {
 interface ModalProps {
   active: boolean;
   onClose: () => void;
+  title: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ active, onClose, children }) => {
+const MotionContent = motion.custom(DialogContent);
+
+// Annoying workaround for DialogOverlay not working with framer-motion
+const MotionOverlay: React.FC<React.ComponentProps<typeof DialogOverlay>> = ({
+  children,
+  ...rest
+}) => {
   return (
-    <Dialog
-      css={(theme: Theme) => css`
-        background-color: ${theme.colors.modalBackground};
-        position: relative;
-        border-radius: 3px;
+    <DialogOverlay
+      css={css`
+        background: none;
       `}
-      isOpen={active}
-      onDismiss={onClose}
+      {...rest}
     >
-      <button
-        css={(theme: Theme) => css`
-          background: none;
-          border: none;
-          position: absolute;
-          top: 1rem;
-          right: 1rem;
-          font-size: 1rem;
-          color: ${theme.colors.green[50].hex};
+      <motion.div
+        css={css`
+          visibility: visible;
+          position: fixed;
+          top: 0;
+          right: 0;
+          left: 0;
+          bottom: 0;
+          background-color: rgba(0, 0, 0, 0.5);
         `}
-        onClick={onClose}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
       >
-        X
-      </button>
-      {children}
-    </Dialog>
+        {children}
+      </motion.div>
+    </DialogOverlay>
+  );
+};
+
+const Modal: React.FC<ModalProps> = ({ active, onClose, title, children }) => {
+  return (
+    <AnimatePresence>
+      {active && (
+        <MotionOverlay key="overlay" onDismiss={onClose}>
+          <MotionContent
+            css={(theme: Theme) => css`
+              background-color: ${theme.colors.modalBackground};
+              position: relative;
+              border-radius: 3px;
+              padding: 1rem;
+            `}
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+          >
+            <header
+              css={css`
+                display: flex;
+              `}
+            >
+              <span>{title}</span>
+              <button
+                css={(theme: Theme) => css`
+                  background: none;
+                  border: none;
+                  margin-left: auto;
+                  color: ${theme.colors.green[50].hex};
+                  font-size: 1rem;
+                `}
+                onClick={onClose}
+              >
+                X
+              </button>
+            </header>
+            {children}
+          </MotionContent>
+        </MotionOverlay>
+      )}
+    </AnimatePresence>
   );
 };
 
--- users/AddUserButton.tsx
+++ users/AddUserButton.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Modal, { useModal } from "../components/modal/Modal";
+import AddUserForm from "./AddUserForm";
 
 const AddUserButton: React.FC = () => {
   const [active, open, close] = useModal();
   return (
     <>
       <button onClick={open}>Add User</button>
-      <Modal active={active} onClose={close}>
-        <h1>Hi!</h1>
+      <Modal title="Add User" active={active} onClose={close}>
+        <AddUserForm />
       </Modal>
     </>
   );
