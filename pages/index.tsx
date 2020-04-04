e704e0c27637fed8a3b7aa2d138e5aa9f92db4f2
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-04-04 17:24:49 -0400

    Style button and transition in new players

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { AppDispatch } from "../store/store";
 import { fetchUsers, userSelectors } from "../users/userSlice";
 import { Theme } from "../styles/theme";
 import AddUserButton from "../users/AddUserButton";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Home: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -26,6 +27,7 @@ const Home: React.FC = () => {
       <div
         css={css`
           display: flex;
+          justify-content: flex-end;
         `}
       >
         <AddUserButton />
@@ -41,35 +43,41 @@ const Home: React.FC = () => {
           }
         `}
       >
-        {users.map((user) => (
-          <li
-            css={(theme: Theme) => css`
-              padding: 0.5rem 1rem;
-              box-shadow: 1px 2px 4px ${theme.colors.blue[80].hex};
-              border-radius: 3px;
-              display: flex;
-              background-color: ${theme.colors.blue[90].hex};
-              color: ${theme.colors.green[20].hex};
-            `}
-            key={user.id}
-          >
-            <span
-              css={css`
-                font-weight: 600;
+        <AnimatePresence initial={false}>
+          {users.map((user) => (
+            <motion.li
+              css={(theme: Theme) => css`
+                padding: 0.5rem 1rem;
+                box-shadow: 1px 2px 4px ${theme.colors.blue[80].hex};
+                border-radius: 3px;
+                display: flex;
+                background-color: ${theme.colors.blue[90].hex};
+                color: ${theme.colors.green[20].hex};
               `}
+              key={user.id}
+              initial={{ scale: 0.8, opacity: 0, x: 0 }}
+              animate={{ scale: 1, opacity: 1, x: 0 }}
+              exit={{ scale: 1, opacity: 0, x: 100 }}
+              positionTransition
             >
-              {user.name}
-            </span>
-            <span
-              css={css`
-                margin-left: auto;
-                opacity: 0.75;
-              `}
-            >
-              {user.rating}
-            </span>
-          </li>
-        ))}
+              <span
+                css={css`
+                  font-weight: 600;
+                `}
+              >
+                {user.name}
+              </span>
+              <span
+                css={css`
+                  margin-left: auto;
+                  opacity: 0.75;
+                `}
+              >
+                {user.rating}
+              </span>
+            </motion.li>
+          ))}
+        </AnimatePresence>
       </ul>
     </div>
   );
--- users/AddUserButton.tsx
+++ users/AddUserButton.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import Modal, { useModal } from "../components/modal/Modal";
 import AddUserForm from "./AddUserForm";
+import { css } from "@emotion/core";
+import { Theme } from "../styles/theme";
 
 const AddUserButton: React.FC = () => {
   const [active, open, close] = useModal();
   return (
     <>
-      <button onClick={open}>Add User</button>
+      <button
+        css={(theme: Theme) => css`
+          background-color: ${theme.colors.green[20].hex};
+          color: ${theme.colors.green[90].hex};
+          border: none;
+          padding: 0.5rem;
+          font-size: 1rem;
+          border-radius: 3px;
+          box-shadow: 1px 2px 4px ${theme.colors.green[90].hex};
+          outline: none;
+
+          :hover,
+          :focus {
+            background-color: ${theme.colors.green[10].hex};
+            box-shadow: 2px 3px 6px ${theme.colors.green[90].hex};
+          }
+        `}
+        onClick={open}
+      >
+        Add User
+      </button>
       <Modal title="Add User" active={active} onClose={close}>
         <AddUserForm onAfterSubmit={close} />
       </Modal>
