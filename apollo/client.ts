42b528c0de5cd18048f6e1857046937626a2119b
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-04-07 20:42:25 -0400

    Fix build issue

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,5 +1,12 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { BatchHttpLink } from "@apollo/link-batch-http";
+import fetch from "isomorphic-fetch";
+
+// Sometimes Creating the client causes issues on the server / at build time
+// because it relies on having a global fetch
+if (typeof window === "undefined") {
+  (global as any).fetch = fetch;
+}
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
