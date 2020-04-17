865782b55a4a83f734691b5609bb58bdbc195296
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-04-17 18:05:00 -0400

    Add default values to uuids

--- migrations/20200414200541_create_users_and_games.ts
+++ migrations/20200414200541_create_users_and_games.ts
@@ -1,8 +1,14 @@
 import type Knex from "knex";
 
 exports.up = async function up(knex: Knex) {
+  await knex.raw('create extension if not exists "uuid-ossp"');
+
   await knex.schema.createTable("users", (table) => {
-    table.uuid("id").primary().notNullable();
+    table
+      .uuid("id")
+      .primary()
+      .notNullable()
+      .defaultTo(knex.raw("uuid_generate_v4()"));
     table.string("name").notNullable();
     table.integer("ladder_rung").notNullable();
     table.integer("streak").notNullable();
@@ -10,7 +16,11 @@ exports.up = async function up(knex: Knex) {
   });
 
   await knex.schema.createTable("games", (table) => {
-    table.uuid("id").primary().notNullable();
+    table
+      .uuid("id")
+      .primary()
+      .notNullable()
+      .defaultTo(knex.raw("uuid_generate_v4()"));
     table.uuid("white_player").notNullable().references("id").inTable("users");
     table.uuid("black_player").notNullable().references("id").inTable("users");
     table
@@ -26,4 +36,5 @@ exports.up = async function up(knex: Knex) {
 exports.down = async function down(knex: Knex) {
   await knex.schema.dropTable("games");
   await knex.schema.dropTable("users");
+  await knex.raw('drop extension if exists "uuid-ossp"');
 };
