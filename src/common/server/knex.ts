25a1322c0e26c1d80490cfdbc3fc13180fad94dd
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-04-18 22:42:20 -0400

    Fix overconfiguration of knex

--- src/common/server/knex.ts
+++ src/common/server/knex.ts
@@ -1,8 +1,15 @@
 import configKnex from "knex";
 
-const knex = configKnex({
-  client: "pg",
-  connection: process.env.DATABASE_CONNECTION_URL,
-});
+function createKnex() {
+  return configKnex({
+    client: "pg",
+    connection: process.env.DATABASE_CONNECTION_URL,
+  });
+}
 
-export default knex;
+let knex: ReturnType<typeof createKnex> | null = null;
+if (!knex) {
+  knex = createKnex();
+}
+
+export default knex!;
