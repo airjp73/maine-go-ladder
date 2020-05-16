8707b100ebc8e14b683034d9db75d9946c3eec80
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-05-16 18:29:33 -0400

    Add testing making sure the correct ladder rung is returned

--- src/api-tests/users.test.ts
+++ src/api-tests/users.test.ts
@@ -3,7 +3,9 @@ import random from "lodash/random";
 import knex from "../common/server/knex";
 import { User } from "../resources/users/User";
 import { getUsers } from "../pages/api/users";
+import { LadderHistoryItem } from "../resources/users/LadderHistoryItem";
 
+// TODO: move these into util files
 function randomString() {
   return (
     Math.random().toString(36).substring(2, 15) +
@@ -13,6 +15,10 @@ function randomString() {
 
 function randomBoolean() {
   return Math.random() >= 0.5;
+}
+
+function randomDate() {
+  return new Date(random(new Date(2012, 1, 1).getTime(), Date.now()));
 }
 
 function generateCollection<T>(
@@ -31,8 +37,18 @@ async function randomUser(): Promise<User> {
   return (await knex("users").insert(user).returning("*"))[0];
 }
 
+async function randomLadderEntry(user: User): Promise<LadderHistoryItem> {
+  const ladderEntry: any = {
+    ladder_rung: random(-50, 50),
+    user: user.id,
+    created_at: randomDate(),
+  };
+  return (await knex("ladder_history").insert(ladderEntry).returning("*"))[0];
+}
+
 describe("get users", () => {
-  afterEach(async () => {
+  beforeEach(async () => {
+    await knex("ladder_history").del();
     await knex("users").del();
   });
 
@@ -41,7 +57,32 @@ describe("get users", () => {
     const expected = users.filter((user) => !user.archived);
     const actual = await getUsers();
 
-    // TODO: Test ladder rung, too
+    expect(actual).toHaveLength(expected.length);
+    actual.forEach((user) => {
+      const match = expected.find(
+        (possibleMatch) => possibleMatch.id === user.id
+      );
+      expect(match).toBeDefined();
+    });
+  });
+
+  it("should return all the latest ladder entries for each user", async () => {
+    const users = await generateCollection(randomUser);
+    const usersWithLadder = await Promise.all(
+      users.map(async (user) => {
+        const ladderEntries = await generateCollection(() =>
+          randomLadderEntry(user)
+        );
+        const latestEntry = ladderEntries.sort((a, b) => {
+          const dateA = new Date(a.created_at);
+          const dateB = new Date(b.created_at);
+          return dateB.getTime() - dateA.getTime();
+        })[0];
+        return { ...user, ladder_rung: latestEntry.ladder_rung };
+      })
+    );
+    const expected = usersWithLadder.filter((user) => !user.archived);
+    const actual = await getUsers();
 
     expect(actual).toHaveLength(expected.length);
     actual.forEach((user) => {
@@ -49,6 +90,7 @@ describe("get users", () => {
         (possibleMatch) => possibleMatch.id === user.id
       );
       expect(match).toBeDefined();
+      expect(match!.ladder_rung).toEqual(user.ladder_rung);
     });
   });
 });
--- src/resources/users/LadderHistoryItem.ts
+++ src/resources/users/LadderHistoryItem.ts
@@ -1,4 +1,5 @@
 export interface LadderHistoryItem {
   ladder_rung: number;
   user: string;
+  created_at: string;
 }
