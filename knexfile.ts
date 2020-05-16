7ae65d6d5b6d3beca65cbd9cbbee5a1529c87a92
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-05-16 14:56:06 -0400

    Rejigger test commands

--- knexfile.ts
+++ knexfile.ts
@@ -2,9 +2,13 @@
 require("dotenv").config();
 require("ts-node/register");
 
+const defaultUrl = process.env.TEST
+  ? "postgresql://localhost:5432/maine_go_ladder_test"
+  : "postgresql://localhost:5432/maine_go_ladder";
+
 module.exports = {
   client: "pg",
-  connection: process.env.DATABASE_CONNECTION_URL,
+  connection: process.env.DATABASE_CONNECTION_URL ?? defaultUrl,
   migrations: {
     extension: "ts",
   },
