a56d6e26ba8a3d89302a29b32282298a8cc6e6d7
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-05-25 21:04:18 -0400

    Test games endpiont

--- src/api-tests/randomUtils.ts
+++ src/api-tests/randomUtils.ts
@@ -3,6 +3,7 @@ import random from "lodash/random";
 import knex from "../common/server/knex";
 import { User, UnsavedUser } from "../resources/users/User";
 import { LadderHistoryItem } from "../resources/users/LadderHistoryItem";
+import { Game, NewGame } from "../resources/games/Game";
 
 export function randomString() {
   return (
@@ -17,6 +18,21 @@ export function randomBoolean() {
 
 export function randomDate() {
   return new Date(random(new Date(2012, 1, 1).getTime(), Date.now()));
+}
+
+export function randomItem<T>(items: T[]): T {
+  expect(items.length).toBeGreaterThanOrEqual(1);
+  const index = random(0, items.length - 1);
+  return items[index];
+}
+
+export function randomItemExcept<T>(items: T[], except: T | T[]): T {
+  const itemsToChoose = items.filter(
+    Array.isArray(except)
+      ? (item) => !except.includes(item)
+      : (item) => item !== except
+  );
+  return randomItem(itemsToChoose);
 }
 
 type UserCustomizer = (user: UnsavedUser) => UnsavedUser;
@@ -53,9 +69,23 @@ export async function randomLadderEntry(
   return (await knex("ladder_history").insert(ladderEntry).returning("*"))[0];
 }
 
+export async function randomGame(users: User[]): Promise<Game> {
+  const winner = randomItem(users);
+  const loser = randomItemExcept(users, winner);
+  const black = randomBoolean() ? winner : loser;
+  const white = winner === black ? loser : winner;
+  const newGame: NewGame = {
+    black: black.id,
+    white: white.id,
+    winner: winner.id,
+  };
+  const gameRecord = (await knex("games").insert(newGame).returning("*"))[0];
+  return { ...gameRecord, black, white };
+}
+
 export function generateCollection<T>(
   supplier: () => Promise<T>,
   num?: number
 ): Promise<T[]> {
-  return Promise.all(range(0, num || random(2, 5)).map(supplier));
+  return Promise.all(range(0, num || random(3, 7)).map(() => supplier()));
 }
