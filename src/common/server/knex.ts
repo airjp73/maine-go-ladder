a0a37752d2a29afbc760428b30a8f24e93cd86e5
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-05-25 23:10:37 -0400

    Make db url logic more correct

--- src/common/server/knex.ts
+++ src/common/server/knex.ts
@@ -1,15 +1,19 @@
 import configKnex from "knex";
 
-const workerId = process.env.JEST_WORKER_ID;
-const defaultUrl =
-  process.env.NODE_ENV === "test"
-    ? `postgresql://localhost:5432/maine_go_ladder_test${workerId}`
-    : "postgresql://localhost:5432/maine_go_ladder";
+const getUrl = () => {
+  const workerId = process.env.JEST_WORKER_ID;
+  const testUrl = `postgresql://localhost:5432/maine_go_ladder_test${workerId}`;
+  const devUrl = "postgresql://localhost:5432/maine_go_ladder";
+  const prodUrl = process.env.DATABASE_CONNECTION_URL;
+
+  if (process.env.NODE_ENV === "test") return testUrl;
+  return prodUrl ?? devUrl;
+};
 
 function createKnex() {
   return configKnex({
     client: "pg",
-    connection: process.env.DATABASE_CONNECTION_URL ?? defaultUrl,
+    connection: getUrl(),
   });
 }
 
