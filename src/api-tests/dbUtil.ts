5966de8f7011050c399c4a16bc250370e7f459ef
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-06-06 18:32:14 -0400

    fix: Clean up audit events between tests

--- src/api-tests/dbUtil.ts
+++ src/api-tests/dbUtil.ts
@@ -4,4 +4,5 @@ export async function cleanupDB(): Promise<void> {
   await knex("ladder_history").del();
   await knex("games").del();
   await knex("users").del();
+  await knex("audit_events").del();
 }
