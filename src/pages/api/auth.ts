475b0dad3eab4370e50eb75d29c9bbae2f3955ab
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-06-06 21:30:28 -0400

    fix: Fix issue where the correct password got a 401

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,6 +1,8 @@
+import * as yup from "yup";
 import cookie from "cookie";
 import jsonwebtoken from "jsonwebtoken";
 import createRequestHandler from "../../common/server/createRequestHandler";
+import BadRequestError from "../../common/server/BadRequestError";
 
 /**
  * A bit of an unorthodox approach to authentication.
@@ -8,12 +10,22 @@ import createRequestHandler from "../../common/server/createRequestHandler";
 const PASSWORD = process.env.LOGIN_PASSWORD;
 const SECRET = process.env.JWT_SECRET;
 
+const bodySchema = yup
+  .object({
+    password: yup.string().required(),
+  })
+  .required();
+
 export default createRequestHandler({
-  POST: (req, res) => {
+  POST: async (req, res) => {
+    const { password } = await bodySchema
+      .validate(req.body)
+      .catch(BadRequestError.throw);
+
     if (!SECRET || !PASSWORD)
       return res.status(500).send("Error with app configuration");
 
-    if (req.body.password === PASSWORD) {
+    if (password === PASSWORD) {
       const jwt = jsonwebtoken.sign({ loginDate: Date.now() }, SECRET);
       res.setHeader("Set-Cookie", cookie.serialize("jwt", jwt));
       return res.status(200).send("Logged In!");
